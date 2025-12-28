d9b093ce67d0b6fa5b03eb285b7ea1d9312c3d97
dossym17 <meiramdosym@gmail.com>
2025-12-28 21:41:10 +0500

    Update main.java

--- main.java
+++ main.java
@@ -1,16 +1,16 @@
-public class Main {
-
+public class main {
     public static void main(String[] args) {
 
         Product p1 = new Product("Milk", 500, "Dairy", true);
-        Product p2 = new Product("Bread", 200, "Bakery", false);
+        Product p2 = new Product("Bread", 200, "Bakery", true);
 
         Sale sale = new Sale(1, 0, 0, false);
         sale.addProduct(p1);
         sale.addProduct(p2);
 
-        Customer customer = new Customer("Dias", 0, false, 1);
+        Customer customer = new Customer(1, "Dias", 0, false);
         customer.addPurchase(sale.getTotal());
+        customer.updateVipStatus();
 
         System.out.println(p1);
         System.out.println(p2);
@@ -18,4 +18,3 @@ public class Main {
         System.out.println(customer);
     }
 }
-
