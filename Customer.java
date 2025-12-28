5e5638b7c902cfe19ae8e947946b07d200e68d04
dossym17 <meiramdosym@gmail.com>
2025-12-28 21:38:51 +0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,61 +1,44 @@
 public class Customer {
 
+    private int customerId;
     private String name;
     private int totalSpent;
     private boolean vip;
-    private int customerId;
 
-    public Customer(String name, int totalSpent, boolean vip, int customerId) {
+    public Customer(int customerId, String name, int totalSpent, boolean vip) {
+        this.customerId = customerId;
         this.name = name;
         this.totalSpent = totalSpent;
         this.vip = vip;
-        this.customerId = customerId;
     }
 
-    public String getName() {
-        return name;
-    }
+    public int getCustomerId() { return customerId; }
+    public String getName() { return name; }
+    public int getTotalSpent() { return totalSpent; }
+    public boolean isVip() { return vip; }
 
-    public int getTotalSpent() {
-        return totalSpent;
-    }
-
-    public boolean isVip() {
-        return vip;
-    }
-
-    public int getCustomerId() {
-        return customerId;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setTotalSpent(int totalSpent) {
-        this.totalSpent = totalSpent;
-    }
-
-    public void setVip(boolean vip) {
-        this.vip = vip;
-    }
-
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
+    public void setCustomerId(int customerId) { this.customerId = customerId; }
+    public void setName(String name) { this.name = name; }
+    public void setTotalSpent(int totalSpent) { this.totalSpent = totalSpent; }
+    public void setVip(boolean vip) { this.vip = vip; }
 
     public void addPurchase(int amount) {
         totalSpent += amount;
-        updateVipStatus();
     }
 
     public void updateVipStatus() {
         vip = totalSpent >= 1000;
     }
 
+    public void resetPurchases() {
+        totalSpent = 0;
+        vip = false;
+    }
+
     @Override
     public String toString() {
-        return "Customer #" + customerId + ": " + name +
-                ", Spent: " + totalSpent + " KZT, VIP: " + vip;
+        return "Customer #" + customerId + ": " + name + ", Spent: " + totalSpent + " KZT, VIP: " + vip;
     }
 }
+
+
